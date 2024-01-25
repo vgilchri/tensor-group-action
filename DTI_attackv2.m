9b2cb4f2a266d1dbe6f71c0556b8c3b630692f6a
lauranemarco <74072839+lauranemarco@users.noreply.github.com>
2024-01-25 10:52:37 +0100

    Create DTI_attackv2.m

---
+++ DTI_attackv2.m
@@ -0,0 +1,30 @@
+clear; 
+
+load "group-action.m";
+
+// attacking DTI variant using rank 0 points version 2 : sampling a random pointand checking whether it has full rank (b=0 case) or not (b=1 case)
+
+
+RankAttack:=function(pk);
+u:= RandomMatrix(F,1,n); // sample a random point 
+M:=ZeroMatrix(F,n);
+for i in [1..n] do 
+    M:=u[1][i]*pk[i];
+end for;
+r:=Rank(M); // check the resulting rank 
+if r eq n then 
+    return 0;
+end if;
+return 1;
+end function;
+
+ctr:=0;
+for i in [1..50] do
+    A,B,C,b:=keygen();
+    pk:=Commitment(A,B,C,b);
+    b1:=RankAttack(pk);
+    if b eq b1 then 
+        ctr+:=1;
+    end if;
+end for;
+ctr;
